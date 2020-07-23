d877f410c39e2b94a4f652c3c5090226ae38570f
kienboeck <kienboec@technikum-wien.at>
2020-07-24 00:46:37 +0200

    introduced logfile with improved logging capabilities (file available via dist-mount)

--- make.py
+++ make.py
@@ -7,42 +7,9 @@ import yaml
 import datetime
 import json
 
-print("______________________________________________________")
-print("")
-print("Slide Crafting (started: " +  str(datetime.datetime.now()) + ")")
-print("______________________________________________________")
-
 #########
 # config
 #########
-def checkEnvValueSet(key, value):
-    return key in os.environ and os.environ[key] == value
-
-generationMethods = []
-if(checkEnvValueSet("GEN_BEAMER", "1")):
-    print("beamer-slides requested")
-    generationMethods.append("beamer")
-
-if(checkEnvValueSet("GEN_BEAMER_NOTES", "1")):
-    print("beamer-slides with notes requested")
-    generationMethods.append("beamerN")
-
-if(checkEnvValueSet("GEN_HANDOUT", "1")):
-    print("handout requested")
-    generationMethods.append("pdf")
-
-if(checkEnvValueSet("GEN_PPTX", "1")):
-    print("powerpoint-slides requested")
-    generationMethods.append("pptx")
-
-if(checkEnvValueSet("GEN_REVEALJS", "1")):
-    print("reveal.js-slides requested")
-    generationMethods.append("revealjs")
-
-if(len(generationMethods) == 0):
-    print("no request... fallback slides with notes")
-    generationMethods = ["beamerN"]
-
 pandocApp = "pandoc"
 pdflatexApp = "pdflatex"
 baseDir = "/miktex/work/"
@@ -50,6 +17,8 @@ originFolder = baseDir + "src/"
 workFolder = baseDir + "tmp/"
 distFolder = baseDir + "dist/"
 logFolder = baseDir + "log/"
+pandocLogFile = logFolder+"pandoc.log"
+logFile = logFolder + "slideCrafting.log"
 pptxReference = originFolder + "_templates/pptx/fhtw_reference.pptx"
 beamerTemplate = originFolder + "_templates/tex/latex/beamer/" # only 1 beamer theme file in the folder supported!
 configFile = baseDir + ".slidecrafting.config"
@@ -58,6 +27,59 @@ templateUpdateScript = "/miktex/work/slideCrafting/updateTemplate.sh"
 viewerHtml = '/miktex/work/slideCrafting/viewer/index.html'
 favicon = '/miktex/work/slideCrafting/viewer/favicon.ico'
 attemptsOnError = 3
+
+######
+# log
+######
+logFileHandler = open(logFile, 'w')
+
+def writeToLog(text):
+    logFileHandler.write(datetime.datetime.now().strftime("%H:%M:%S") + ": " + text + "\n")
+    logFileHandler.flush()
+
+def writeToScreen(text):
+    print(text)
+
+def writeToLogAndScreen(text):
+    writeToLog(text)
+    writeToScreen(text)
+
+writeToScreen("\n\n\n\n\n")
+writeToLogAndScreen("______________________________________________________")
+writeToLogAndScreen("")
+writeToLogAndScreen("Slide Crafting (started: " +  str(datetime.datetime.now()) + ")")
+writeToLogAndScreen("______________________________________________________")
+
+###################
+# read gen options
+###################
+def checkEnvValueSet(key, value):
+    return key in os.environ and os.environ[key] == value
+
+generationMethods = []
+if(checkEnvValueSet("GEN_BEAMER", "1")):
+    writeToLog("beamer-slides requested")
+    generationMethods.append("beamer")
+
+if(checkEnvValueSet("GEN_BEAMER_NOTES", "1")):
+    writeToLog("beamer-slides with notes requested")
+    generationMethods.append("beamerN")
+
+if(checkEnvValueSet("GEN_HANDOUT", "1")):
+    writeToLog("handout requested")
+    generationMethods.append("pdf")
+
+if(checkEnvValueSet("GEN_PPTX", "1")):
+    writeToLog("powerpoint-slides requested")
+    generationMethods.append("pptx")
+
+if(checkEnvValueSet("GEN_REVEALJS", "1")):
+    writeToLog("reveal.js-slides requested")
+    generationMethods.append("revealjs")
+
+if(len(generationMethods) == 0):
+    writeToLog("no request... fallback slides with notes")
+    generationMethods = ["beamerN"]
 
 ########
 # clean
@@ -80,17 +102,17 @@ if os.path.isdir(distFolder):
     time.sleep(0.1) 
 else:
     os.mkdir(distFolder)
-print("dist folder cleaned..." + distFolder)
+writeToLog("dist folder cleaned..." + distFolder)
 
 if os.path.isdir(workFolder):
     rmdirRecursive(workFolder)
     time.sleep(0.1)
-print("work folder cleaned..." + workFolder)
+writeToLog("work folder cleaned..." + workFolder)
 
 ##########
 # viewer
 ##########
-print("copy viewer")
+writeToLog("copy viewer")
 subprocess.call(["cp", viewerHtml, distFolder + "index.html"])
 subprocess.call(["cp", favicon, distFolder + "favicon.ico"])
 
@@ -132,7 +154,7 @@ pandocErrors = {
 # is mounted as a read-only volume)
 subprocess.call(["cp", "-r", originFolder, workFolder])
 time.sleep(0.1)
-print("work folder filled with source data...")
+writeToLog("work folder filled with source data...")
 
 #######################
 # search index files
@@ -143,14 +165,15 @@ version = {}
 
 for file in os.listdir(workFolder):
     if file.endswith(indexFilesExtension):
-        print("index file found: " + file)
+        writeToLogAndScreen("index file found: " + file)
         stream = open(workFolder + file, 'r')
         indexFileContent = yaml.load(stream, Loader=yaml.FullLoader)
         version[file] = str(indexFileContent['version'])
         topicsDict[file] = indexFileContent['input-files']
         stream.close()
-        print("   files: ", topicsDict[file])
-print("searching for index files completed")
+        writeToLog("   files: " + ", ".join(topicsDict[file]))
+
+writeToLog("searching for index files completed")
 
 ###############
 # build config
@@ -162,7 +185,7 @@ if os.path.isdir(beamerTemplate):
         if file.startswith("beamertheme") and file.endswith(".sty"):
             themeName = file[11:-4]
             themeArgs = ["-V", "theme:" + themeName]
-            print("theme applied: "+ themeName)
+            writeToLog("theme applied: "+ themeName)
             break
 
 needToWriteConfig = False
@@ -177,19 +200,22 @@ else:
 needToWriteConfig = needToWriteConfig or storedConfig.get("template") != themeName
 
 if needToWriteConfig:
-    print("updating template")
-    subprocess.call([templateUpdateScript])
+    writeToLog("updating template")
+    executionResult = subprocess.run([templateUpdateScript], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    writeToLog("standard out: \n" + executionResult.stdout)
+    writeToLog("standard err: \n" + executionResult.stderr)
+
     f = open(configFile, "w")
     f.write("template: " + themeName)
     f.close()
-    print("wrote config")
+    writeToLog("wrote config")
 else:
-    print("config up to date...")
+    writeToLog("config up to date...")
 
 pandocArgs = [
     # "--verbose",
     "--from=markdown+raw_tex+header_attributes+implicit_header_references+raw_attribute+inline_code_attributes+fancy_lists+line_blocks", 
-    "--log="+logFolder+"pandoc.log",
+    "--log="+pandocLogFile,
     # "--toc", # table of content
     # "--toc-depth=2",
     "--highlight-style=tango",
@@ -249,19 +275,48 @@ for fileName in topicsDict.keys():
             exitCode = ""
             attempt = 1
             while exitCode != "0" and attempt <= attemptsOnError:
-                print ("---------------------------------------------- start: " + str(datetime.datetime.now()))
+                writeToLogAndScreen ("---------------------------------------------- start    : " + str(datetime.datetime.now()))
                 outputFileName = projectArgs[mode][projectArgs[mode].index('-o')+1]
                 outputFileNames.append(outputFileName)
-                print(mode + ": (file:" + outputFileName + ", version: " + version[fileName] + ", attempt: " + str(attempt) + ")")
-                exitCode = str(subprocess.call(projectArgs[mode]))
+                writeToLogAndScreen(mode + ": (file:" + outputFileName + ", version: " + version[fileName] + ", attempt: " + str(attempt) + ")")
+                executionResult = subprocess.run(projectArgs[mode], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+                
+                if(len(executionResult.stdout) > 0):
+                    first = True
+                    for line in executionResult.stdout.splitlines():
+                        patchedLine = line.strip()
+                        if len(patchedLine) > 0:
+                            if first:
+                                writeToLogAndScreen("standard out:")
+                                first=False
+                            writeToLogAndScreen(" -" + patchedLine)
+                    
+                if(len(executionResult.stderr) > 0):
+                    first = True
+                    for line in executionResult.stderr.splitlines():
+                        # i accept these warnings and remove it from the error output                
+                        patchedLine = line.replace("pdflatex: security risk: running with elevated privileges","").replace("pdflatex: major issue: So far, no MiKTeX administrator has checked for updates.", "").strip()
+                        
+                        if len(patchedLine) > 0:
+                            if first:
+                                writeToLogAndScreen("standard error:")
+                                first=False
+                            writeToLogAndScreen(" -" + patchedLine)
+                    
+
+                exitCode = str(executionResult.returncode)
                 exitCodeDescription = "error"
-                if exitCode in pandocErrors:
-                    exitCodeDescription = pandocErrors[exitCode]
-                print ("exit code: " + exitCode + " (" + exitCodeDescription + ")")
-                print ("---------------------------------------------- end  : " + str(datetime.datetime.now()))
+                
+                if exitCode != "0":
+                    if exitCode in pandocErrors:
+                        exitCodeDescription = pandocErrors[exitCode]
+                    writeToLogAndScreen ("exit code: " + exitCode + " (" + exitCodeDescription + ")")
+                    writeToLogAndScreen ("                                               failed   : " + str(datetime.datetime.now()))
+                else:
+                    writeToLogAndScreen ("                                               succeeded: " + str(datetime.datetime.now()))
                 attempt += 1
             
-print("Files created", outputFileNames)
+writeToLogAndScreen("Files created: " + ",\n".join(outputFileNames))
 outputFileNameFile = open(distFolder+"files.js","w")
 isFirst = True
 
@@ -274,3 +329,7 @@ for outputFileName in outputFileNames:
 
 outputFileNameFile.write("\n]")
 outputFileNameFile.close()
+
+logFileHandler.close()
+time.sleep(0.1) 
+subprocess.call(["cp", logFile, distFolder + "slideCrafting.log"])
